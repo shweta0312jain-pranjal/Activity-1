63a623937d7ac8d1dc6c22d5028a6fd6ff3a6b98
shweta0312jain-pranjal <shweta0312jain@gmail.com>
2025-12-30 18:43:44 +0530

    Add files via upload

---
+++ Activity.py
@@ -0,0 +1,8 @@
+print("What is the type of above given database  -\n 1) Relational Database \n 2) Non-Relational Database")
+answer = int(input("Enter your choice (1 or 2): "))
+if answer == 2:
+    print("Correct Answer")
+else:
+    print("Wrong Answer")
+
+    print("\n Tell again")
